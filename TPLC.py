47f1ffbebb9dbfc2766568a0acbec6633a9b617e
Edward Russell <erussell@ciservicesnow.com>
2020-07-30 16:35:05 -0400

    Added GetItems, GetItem, and UpdateItem

--- TPLC.py
+++ TPLC.py
@@ -139,6 +139,98 @@ def GetInventory(pgsiz=100, pgnum=1, rql="", sort="", senameorvaluecontains="" )
 
     return items
 
+def GetItems(customer_id, pgsiz=100, pgnum=1, rql="", sort=""):
+    options = {
+        "pgsiz": pgsiz,
+        "pgnum": pgnum,
+        "rql": rql,
+        "sort": sort,
+    }
+    options = {k:v for k,v in options.items() if v}
+    options = urlencode(options)
+    base_url = f"https://secure-wms.com"
+    url = f"{base_url}/customers/{customer_id}/items?{options}"
+
+    headers = {
+        "Host"              : "secure-wms.com",
+        "Content-Type"      : "application/hal+json; charset=utf-8",
+        "Accept"            : "application/hal+json",
+        "Authorization"     : f"Bearer {access_token}"
+    }
+
+    response = requests.get(url=url, headers=headers)
+    data = response.json()
+
+    total_results = data["totalResults"]
+    current_progress = 0
+    items = []
+
+    while True:
+
+        print(f'Gathering page {pgnum}.\t{current_progress} out of {total_results} [{current_progress/total_results:2.2%}]')
+
+        response = requests.get(url=url, headers=headers)
+        
+        if response.status_code != 200:
+            raise Exception
+
+        data = response.json()
+        items += data['_embedded']['http://api.3plCentral.com/rels/customers/item']
+
+        current_progress += pgsiz
+        pgnum += 1
+    
+        if data.get("_links").get("next"):
+            url = f'{base_url}{data.get("_links").get("next").get("href")}'
+        else:
+            break
+
+    return items
+
+def GetItem(customer_id, item_id):
+    base_url = f"https://secure-wms.com"
+    url = f"{base_url}/customers/{customer_id}/items/{item_id}"
+
+    headers = {
+        "Host"              : "secure-wms.com",
+        "Content-Type"      : "application/hal+json; charset=utf-8",
+        "Accept"            : "application/hal+json",
+        "Authorization"     : f"Bearer {access_token}"
+    }
+
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 200:
+        raise Exception
+
+    item = response.json()
+
+    return (item, response.headers["ETag"])
+
+def UpdateItem(customer_id, item_id, etag, payload):
+    base_url = f"https://secure-wms.com"
+    url = f"{base_url}/customers/{customer_id}/items/{item_id}"
+
+    headers = {
+        "Host"              : "secure-wms.com",
+        "Content-Type"      : "application/hal+json; charset=utf-8",
+        "Accept"            : "application/hal+json",
+        "Authorization"     : f"Bearer {access_token}",
+        "If-Match"          : f"{etag}"
+    }
+
+    try:
+        del payload["_links"]
+        del payload["_embedded"]
+    except:
+        pass
+
+    response = requests.put(url=url, headers=headers, data=json.dumps(payload))
+    if response.status_code != 200:
+        raise Exception
+
+
+    return True
+
 global access_token
 access_token = GetAccessToken(tpl_id, tpl_secret, tpl_guid, tpl_user_id)
 
@@ -146,6 +238,9 @@ access_token = GetAccessToken(tpl_id, tpl_secret, tpl_guid, tpl_user_id)
 # testing code below
 if __name__ == '__main__':
 
-    i = GetInventory(pgsiz=1000)
+    item, etag = GetItem(1, 8504)
+    item["sku"] = "04719-test"
+    UpdateItem(1, item["itemId"], etag, item)
+
     print("")
     
